baaf5b4f4e0afd87fad850af4f9dc407f2e03679
axellindstrom <axel.uhjin@gmail.com>
2023-03-07 16:12:36 +0100

    Script plot_map now functional. Plotting map, markers for each related population, cluster close markers and show lable if hovering mous ov marker

--- plot_map.R
+++ plot_map.R
@@ -5,69 +5,84 @@ rm(list=ls())
 #library(maps)
 library(leaflet)
 
+# function to set color of marker based on IBD
+getColor <- function(geo_IBD_data) {
+  sapply(geo_IBD_data[,3], function(x) {
+    if(x <= 0.5) {
+      "green"
+    } else if(x <= 1) {
+      "orange"
+    } else {
+      "red"
+    } })
+}
+# Import geo and IBD data
+geo <- read.csv('./1_Filtered_data/pop_geo.csv', row.names = 1)
+IBD <- read.csv('./1_Filtered_data/filtered_inter.csv')
 
-# Import geo data
-geo <- read.csv('./Geo_data/pop_geo.csv', row.names = 1)
-IBD <- read.csv('./Raw_Data/inter_pop_mean_pairwise_ibd_length.csv')
-
-
-# Plot empty world map
-#par(mar=c(0,0,0,0))
-#maps::map('',
-#          col="#f2f2f2", fill=TRUE, bg="white", lwd=0.05,
-#          mar=rep(0,4),border=0, ylim=c(-80,80) 
-#)
-
-#draw_pop_location <- function(pop_name){
-#  points(x=geo[pop_name,]$long, y=geo[pop_name,]$lat, col="red", cex=3, pch=20)
-  
-#}
-
-#draw_pop_location('Moldova-AJ')
-
-
-
+# Chose population 
 input <- 'Swede'
 
-matcher <- IBD[IBD[,1] == input,2:3]
+# Extract data from chosen population
+IBD_matcher <- IBD[IBD[,1] == input,2:3]
+matching_geo <- na.omit(geo[IBD_matcher[,1],])
 
-# Same stuff but using the %>% operator
+# Order population names
+IBD_matcher <- IBD_matcher[order(IBD_matcher$pop2),]
+matching_geo <- matching_geo[order(rownames(matching_geo)),]
+
+IBD_matcher$pop2 == row.names(matching_geo)
+
+geo_IBD_data <- cbind(matching_geo, IBD_matcher$mean_pairwise_IBD_length)
+
+
+
+# Set icon style and color based on IBD value
+icons <- awesomeIcons(
+  icon = 'ios-close',
+  iconColor = 'black',
+  library = 'ion',
+  markerColor = getColor(geo_IBD_data))
+
+
+# Extract all geo locations from populations of interest
+lines_data_frame <- data.frame()
+for(i in 1:nrow(geo_IBD_data)){
+  new_xy <- c(geo_IBD_data[i,1], geo_IBD_data[i,2])
+  home_xy <- c(geo[input,]$lat, geo[input,]$long)
+  lines_data_frame <- rbind(lines_data_frame, home_xy)
+  lines_data_frame <- rbind(lines_data_frame, new_xy)
+}
+
+# Plot world map
 m <- leaflet() %>% 
   addTiles() %>%
+  
+  # Add red circle to location of chosen population
   addCircleMarkers(lng = geo[input,]$long, 
                    lat = geo[input,]$lat,
                    color = 'red',
                    opacity = 0.7,
                    label = input
-                   )# %>%
-  #addCircleMarkers(lng = geo[matcher[,1],]$long, 
-   #                lat = geo[matcher[,1],]$lat,
-    #               color = 'blue',
-     #              opacity = 0.7,
-      #             label = matcher)
+                   ) %>%
+  # Add marker to all population related to chosen population
+  addAwesomeMarkers(lng = geo_IBD_data$long, 
+                    lat = geo_IBD_data$lat,
+                    labelOptions = labelOptions(textsize = "12px",
+                                                style = list("color" = 'darkgray')),
+                    icon=icons,
+                    
+                    # Cluster markers close together
+                    clusterOptions = markerClusterOptions(),
+                    label = paste(rownames(geo_IBD_data),
+                                  ':', 
+                                  geo_IBD_data$`IBD_matcher$mean_pairwise_IBD_length`,
+                                  'cM')) #%>%
+  
+  # Add line from chosen population to related populations
+ #addPolylines(lat = lines_data_frame[,1],
+  #             lng = lines_data_frame[,2])
 
-
-for (i in 1:nrow(geo[matcher[,1],])){
-  lable_lenght <- matcher[i, 1:2]
-  m <- addCircleMarkers(m, lng = geo[matcher[i,1],]$long, 
-                   lat = geo[matcher[i,1],]$lat,
-                   color = 'blue',
-                   opacity = 0.7,
-                   label = paste(lable_lenght[,1],':', lable_lenght[2], 'cM'),
-                   labelOptions = labelOptions(textsize = "12px",
-                                               style = list("color" = 'darkgray')),
-                   clusterOptions = T )
-} 
-
-
-
-
-for(i in 1:nrow(geo[matcher[,1],])){
-  xy_lines <- c(geo[input,]$lat, geo[input,]$long, geo[matcher[,1],][i,1], geo[matcher[,1],][i,2])
-
-  m <- addPolylines(m, lat = as.numeric(xy_lines[c(1, 3)]), 
-                       lng = as.numeric(xy_lines[c(2, 4)]))
-}
 m
 
 
