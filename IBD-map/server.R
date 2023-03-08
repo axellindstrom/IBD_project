5d56a69d0e986d111fca33900f38d50a10b23eca
axellindstrom <axel.uhjin@gmail.com>
2023-03-08 11:08:40 +0100

    Tabs added to the aplication to display different kind of data to the user. sliding bar is added to let the user filter the data by IBD.

--- IBD-map/server.R
+++ IBD-map/server.R
@@ -10,9 +10,19 @@
 library(shiny)
 source("../plot_map.R")
 library(leaflet)
- 
+
 # Define server logic required to draw a histogram
 function(input, output, session) {
-  output$mymap <- renderLeaflet(plot_map_data(input$Population))
-  
+  output$mymap <- renderLeaflet(plot_map_data(input$Population, input$range))
+  output$quantiles <- renderTable({
+    df <- t(as.data.frame(get_quantiles(input$Population)))
+    rownames(df) <- 'IBD legnth (cM)'
+    return(df)
+    })
+  output$selected_var <- renderText({ 
+    paste("You have selected", input$range)
+  })
+  output$Quantiles <- renderText({
+    print('Quantiles')
+  })
 }
--- IBD-map/ui.R
+++ IBD-map/ui.R
@@ -12,7 +12,20 @@ source("../plot_map.R")
 library(leaflet)
 # Define UI for application that draws a histogram
 fluidPage(
-  leafletOutput("mymap"),
-  selectInput("Population", "Select population", c("Population" = "Swede", population_id))
-  
+  mainPanel(
+    titlePanel("IBD distance"),
+    tabsetPanel(
+      tabPanel("Map", leafletOutput("mymap")), 
+      tabPanel("Summary", 
+               textOutput("Quantiles"),
+               tableOutput("quantiles")), 
+      tabPanel("Table", tableOutput("table")),
+      tabPanel('test', textOutput('selected_var'))
+    )),
+    
+  #leafletOutput("mymap"),
+  selectInput("Population", "Select population", c("Choose a population" = "", population_id)),
+  sliderInput("range", 
+              label = "Filter by IBD lenght:",
+              min = 0, max = 100, value = c(0, 100))
 )
